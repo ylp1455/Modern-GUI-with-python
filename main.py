8bc0815dd3aeb025a9b30bbfbe05ecac71a07d61
Yasiru Perera <yasiruperera682@gmail.com>
2023-06-20 00:04:05 +0530

    root created

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import customtikinter
 customtikinter.set_appearance_mode("dark")
 # you can use system or light for the mode
 
+customtikinter.set_defult_color_theme("dark-blue")
+# you can also set green insted blue
+
+
+root = customtkinter.CTl()
